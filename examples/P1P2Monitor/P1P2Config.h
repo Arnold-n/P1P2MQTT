41ffd7bc8b147d22e1f3908d2d134072c2516bc6
Arnold <48472227+Arnold-n@users.noreply.github.com>
2022-12-25 09:55:41 +0100

    Update P1P2Config.h
    Reported version nr change

--- examples/P1P2Monitor/P1P2Config.h
+++ examples/P1P2Monitor/P1P2Config.h
@@ -2,6 +2,7 @@
  *
  * Copyright (c) 2019-2022 Arnold Niessen, arnold.niessen-at-gmail-dot-com - licensed under CC BY-NC-ND 4.0 with exceptions (see LICENSE.md)
  *
+ * 20221224 v0.9.30 expand F series write possibilities
  * 20221211 v0.9.29 add control_ID in bool in pseudopacket, fix 3+4-byte writes, FXMQ support
  * 20221129 v0.9.28 option to insert message in 40F030 time slot for restart or user-defined write message
  * 20221102 v0.9.24 suppress repeated "too long" warnings
@@ -59,7 +60,7 @@
 #define SERIAL_MAGICSTRING "1P2P" // Serial input line should start with SERIAL_MAGICSTRING, otherwise input line is ignored
 #endif /* F_CPU */
 
-#define WELCOMESTRING "* P1P2Monitor-v0.9.29"
+#define WELCOMESTRING "* P1P2Monitor-v0.9.30"
 
 #define INIT_VERBOSE 3
 // Set verbosity level
